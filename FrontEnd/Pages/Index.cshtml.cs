5d5d3016946f3eca4bcf93edaf43cb70529c74a2
Yordan Rashkov <jrashkov@gmail.com>
2020-12-03 20:43:55 +0200

    List the sessions on the home page
    Load the data into the PageModel, Render the sessions list on the home page

--- FrontEnd/Pages/Index.cshtml.cs
+++ FrontEnd/Pages/Index.cshtml.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ConferenceDTO;
+using FrontEnd.Services;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using System;
@@ -11,15 +13,40 @@ namespace FrontEnd.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        protected readonly IApiClient _apiClient;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IApiClient apiClient)
         {
             _logger = logger;
+            _apiClient = apiClient;
         }
 
-        public void OnGet()
-        {
+        public IEnumerable<IGrouping<DateTimeOffset?, SessionResponse>> Sessions { get; set; }
 
+        public IEnumerable<(int Offset, DayOfWeek? DayOfWeek)> DayOffsets { get; set; }
+
+        public int CurrentDayOffset { get; set; }
+
+        public async Task OnGet(int day = 0)
+        {
+            CurrentDayOffset = day;
+
+            var sessions = await _apiClient.GetSessionsAsync();
+
+            var startDate = sessions.Min(s => s.StartTime?.Date);
+
+            var offset = 0;
+            DayOffsets = sessions.Select(s => s.StartTime?.Date)
+                                    .Distinct()
+                                    .OrderBy(d => d)
+                                    .Select(day => (offset++, day?.DayOfWeek));
+
+            var filterDate = startDate?.AddDays(day);
+
+            this.Sessions = sessions.Where(s => s.StartTime?.Date == filterDate)
+                                    .OrderBy(s => s.TrackId)
+                                    .GroupBy(s => s.StartTime)
+                                    .OrderBy(g => g.Key);
         }
     }
 }
